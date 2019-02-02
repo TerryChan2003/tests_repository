986352d63aa8b5cd7bca98880e97ce2ab0d387d6
Aleksandr Kharitonov <aleksandr_h2001@mail.ru>
2019-02-02 15:58:35 +0300

    add json reader function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
-#[warn(dead_code)]
+#![allow(dead_code, unused_imports)]
 extern crate reqwest;
+extern crate serde_json;
 
+use serde_json::{Result, Value, from_reader, Deserializer};
+use serde_json::from_str;
+
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
 use std::io;
 use std::collections::HashMap;
+
 fn http_get(url: &str, params: HashMap<&str, &str>) -> String
 {
     let mut param = String::new();
@@ -19,6 +28,13 @@ fn http_get(url: &str, params: HashMap<&str, &str>) -> String
     req.text().unwrap()
 }
 
+fn get_json_data(filename: &str) -> Value
+{
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+    serde_json::from_reader(reader).unwrap()
+}
+
 fn input<T>(text: T) -> String 
     where T: ToString
 {
@@ -33,6 +49,8 @@ fn main() {
     map.insert("login", "testing");
     map.insert("password", "12345");
     let t = http_get("http://httpbin.org/get", map);
-    println!("Result: {}", t);
+    println!("Result http: {}", t);
+    let t = get_json_data("login.json");
+    println!("Result reader: {}", t);
     let _t = input("Click enter for close...");
 }
