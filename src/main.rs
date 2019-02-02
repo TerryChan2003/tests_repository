66e94ccc3aa92ae97d8c7a18dcab862a1b6334cb
Aleksandr Kharitonov <aleksandr_h2001@mail.ru>
2019-02-02 12:10:32 +0300

    add function http_get

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 #[warn(dead_code)]
+extern crate reqwest;
+
 use std::io;
 use std::string::String;
 use std::string::ToString;
+fn http_get<T>(url: T) -> String
+    where T: reqwest::IntoUrl
+{
+    let mut req = reqwest::get(url).unwrap();
+    req.text().unwrap()
+}
 
 fn input<T>(text: T) -> String 
     where T: ToString
@@ -13,7 +21,7 @@ fn input<T>(text: T) -> String
 }
 
 fn main() {
-    let s = input("Выпишите число: ");
-    let s : u32 = s.parse().unwrap(); 
-    println!("Ваше введенное число в квадрате {}", s*s);
+    let t = http_get("http://httpbin.org/get");
+    println!("Result: {}", t);
+    let _t = input("Click enter for close...");
 }
